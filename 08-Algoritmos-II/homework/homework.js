e8e751d65a49cbb4b5929bf23c7080ef83075712
Seb-RM <srmmoto@gmail.com>
2023-09-29 22:32:09 -0300

    Anotaciones

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -21,11 +21,11 @@ function quickSort(array) {
         else if (array[i]<pivote)arrayIzq.push(array[i]);
     }
 
-    if(arrayDer.length>1) quickSort(arrayDer);
+    if(arrayDer.length>1) arrayDer = quickSort(arrayDer);
 
-    if(arrayIzq.length>1) quickSort(arrayIzq);
+    if(arrayIzq.length>1) arrayIzq = quickSort(arrayIzq);
 
-    arrayOrdenado = arrayIzq.concat(arrayIgu.concat(arrayDer));
+    arrayOrdenado = arrayIzq.concat(arrayIgu.concat(arrayDer));// quicksort(left).concat(equals).concat(quicksort(right)); 
     return arrayOrdenado;
 }
 
@@ -34,6 +34,25 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+
+  if(array.length < 2) return array;
+
+    let mitad = Math.floor(array.length / 2);// puede ser const
+    let left = array.slice(0, mitad);
+    let right = array.slice(mitad);
+
+    array = [];// modifica directamente el array que se pasa como argumento
+
+    left = mergeSort(left);
+    right = mergeSort(right);
+    
+    while(left.length && right.length){ // tambien se puede escribir así
+    if (left[0] < right[0]) {           // left[0] < right[0] ? array.push(left.shift()) : array.push(right.shift()); 
+        array.push(left.shift());
+    } else array.push(right.shift());
+    }
+    return array.concat(left, right);
+
 
   //okey probemos ahora
 }
--- 08-Algoritmos-II/homework/pruebadecodigo.js
+++ 08-Algoritmos-II/homework/pruebadecodigo.js
@@ -3,33 +3,63 @@
 //Separamos el array en dos, acomodando los elementos deacuerdo a si sus valores son mayores o menores al pivote
 
 
-function quickSort(array) {
+// function quickSort(array) {
 
-    if(array.length<=1) return array;
-    let arrayDer=[];
-    let arrayIzq=[];
-    let arrayIgu=[]
-    let arrayOrdenado=[]
-    let index= array[Math.floor(Math.random() * array.length)];
+//     if(array.length<=1) return array;
+//     let arrayDer=[];
+//     let arrayIzq=[];
+//     let arrayIgu=[]
+//     let arrayOrdenado=[]
+//     let index= array[Math.floor(Math.random() * array.length)];
 
-    let pivote= array[index];
+//     let pivote= array[index];
 
-    for(let i=0;i<array.length;i++){
-        if(array[i]<pivote) arrayIzq.push(array[i]);
-        else if (array[i]>pivote) arrayDer.push(array[i]);
-        else  arrayIgu.push(array[i]);
+//     for(let i=0;i<array.length;i++){
+//         if(array[i]<pivote) arrayIzq.push(array[i]);
+//         else if (array[i]>pivote) arrayDer.push(array[i]);
+//         else  arrayIgu.push(array[i]);
+//     }
+
+//     if(arrayDer.length>1) quickSort(arrayDer);
+
+//     if(arrayIzq.length>1) quickSort(arrayIzq);
+
+//     arrayOrdenado = [...quickSort(arrayIzq), ...arrayIgu, ...quickSort(arrayDer)]
+//     return arrayOrdenado;
+// };
+
+
+// console.log(quickSort([5, 1, 4, 2, 3]));
+// console.log(quickSort([10, 10, 16, 12]));
+// console.log(quickSort([10, -2, -7, 4]));
+
+function mergeSort(array) {
+    // Implementar el método conocido como mergeSort para ordenar de menor a mayor
+    // el array recibido como parámetro
+    // Devolver el array ordenado resultante
+    // Tu código:
+
+    if(array.length < 2) return array;
+
+    let mitad = Math.floor(array.length / 2);
+    let left = array.slice(0, mitad);
+    let right = array.slice(mitad);
+
+    array = [];
+
+    left = mergeSort(left);
+    right = mergeSort(right);
+    
+    while(left.length && right.length){
+    if (left[0] < right[0]) {
+        array.push(left.shift());
+    } else array.push(right.shift());
     }
+    return array.concat(left, right);
 
-    if(arrayDer.length>1) quickSort(arrayDer);
-
-    if(arrayIzq.length>1) quickSort(arrayIzq);
-
-    arrayOrdenado = [...quickSort(arrayIzq), ...arrayIgu, ...quickSort(arrayDer)]
-    return arrayOrdenado;
-};
-
-
-console.log(quickSort([5, 1, 4, 2, 3]));
-console.log(quickSort([10, 10, 16, 12]));
-console.log(quickSort([10, -2, -7, 4]));
+    //okey probemos ahora
+}
+console.log(mergeSort([5, 1, 4, 2, 3]));
+console.log(mergeSort([10, 10, 16, 12]));
+console.log(mergeSort([10, -2, -7, 4]));
 
